5955925f0b7a0cd016d43b27c630725ca1276fbb
eza roointan <m.roointan.10@gmail.com>
2024-02-24 15:46:28 +0330

    ref: Add some comment in UserService

--- src/DoIt.Core/Services/UserService.cs
+++ src/DoIt.Core/Services/UserService.cs
@@ -13,15 +13,18 @@ namespace DoIt.Core.Services
 {
     public class UserService : IUserService
     {
+        // Database context
         private readonly DoItDbContext _db;
 
         public UserService(DoItDbContext dbContext)
         {
+            // Inject and initialize db context instance
             _db = dbContext;
         }
 
         public int RegisterUser(RegisterViewModel viewModel)
         {
+            // Create new user by using view model data
             User user = new()
             {
                 Name = viewModel.Name,
@@ -29,12 +32,14 @@ namespace DoIt.Core.Services
                 Password = HashHelper.GetSha256(viewModel.Password)
             };
 
+            // Add new user into database
             _db.Users.Add(user);
             return _db.SaveChanges();
         }
 
         public bool UsernameExist(string username)
         {
+            // check usernames in database is match with input username 
             return _db.Users.Any(x => x.Username == username.Trim());
         }
     }
